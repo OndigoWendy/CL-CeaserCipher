ea2d4cacf187ff0b23a0c5fd7eff41e8f8cd818e
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-09-05 01:52:31 +0300

    Connect to the business logic with the appropriate method.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,29 +1,32 @@
+
+import javax.sound.sampled.AudioFormat;
 import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         Scanner getInput = new Scanner(System.in);
         System.out.println("Enter service:\n 1.Encryption \n 2.Decryption \n 3.Exit");
 
         int option = getInput.nextInt();
-    //   option.nextLine();
+        getInput.nextLine();
 
         if (option == 1)
         {
             System.out.println("Enter message to encrypt:");
-           // String.inputText =getInput.nextLine().toUpperCase();
+         String inputText =getInput.nextLine().toUpperCase();
             System.out.println("Enter Shift Key:");
-            int ShiftKey = getInput.nextInt();
-         //   Encoding encryptInput = new Encoding(inputText,ShiftKey);
-          //  System.out.println(String.format("Your input has been encrypted as:%s",encoding.encode()));
+            int shiftKey = getInput.nextInt();
+          Encoding encoding = new Encoding(inputText,shiftKey);
+           System.out.println(String.format("Your input has been encrypted as:%s",encoding.encode()));
         }
         else if (option == 2)
         {
             System.out.println("Enter message to decrypt:");
-            // String.inputText =getInput.nextLine().toUpperCase();
+            String inputText =getInput.nextLine().toUpperCase();
             System.out.println("Enter Shift Key:");
-            int ShiftKey = getInput.nextInt();
-            //   Decoding decryptInput = new Decoding(inputText,ShiftKey);
-            //  System.out.println(String.format("Your input has been decrypted as:%s",decoding.decode()));
+            int shiftKey = getInput.nextInt();
+            Decoding decoding = new Decoding(shiftKey,inputText);
+             System.out.println(String.format("Your input has been decrypted as:%s",decoding.decode()));
         }
         else if (option == 3)
         {
