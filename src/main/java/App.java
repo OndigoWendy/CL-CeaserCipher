b7ec4f4a702fb25ac3f18701da715dd5c683dece
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-09-04 15:11:10 +0300

    Specify Object name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner getInput = new Scanner(System.in);
+        System.out.println("Enter service:\n 1.Encryption \n 2.Decryption \n 3.Exit");
     }
 }
